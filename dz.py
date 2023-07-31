334b892aefeaa39b3b99ef0d15e1eae7ab2b969e
nurya <nuryarmukhamedov@mail.ru>
2023-07-31 17:54:26 +0300

    Проверка палиндрома осуществляется с помощью срезов в питон, .lower() исп ользуется для предотвращия ошибки о том,что слово не палндром потому что одинаковые буквы , но одна-заглавная,а другая - строчная Изменил все по требованиям

--- dz.py
+++ dz.py
@@ -1,6 +1,9 @@
-s = input().lower()
-print(s)
-if s == s[::-1]:
-    print('True')
-else:
-    print('False')
+
+def is_palindrome(slovo):
+    ishodnoe_slovo = slovo.lower()
+    obrabotanoe_slovo = ishodnoe_slovo[::-1]
+    if ishodnoe_slovo == obrabotanoe_slovo :
+        return True
+    else:
+        return  False
+
